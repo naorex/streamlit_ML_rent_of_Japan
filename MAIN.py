7963adc127794cf638d100e4edb152ee899d59cc
naorex <nao23r@gmail.com>
2023-08-14 19:26:16 +0900

    page title set

--- MAIN.py
+++ MAIN.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
+st.set_page_config(
+        page_title='不動産賃貸 家賃推定',
+        page_icon="🏠"
+        )
+
 # Streamlitのマルチページ機能を使用
-st.title('不動産賃貸 家賃予測')
-st.subheader('機械学習による家賃予測プログラム')
+st.title('不動産賃貸 家賃推定')
+st.subheader('機械学習による家賃推定プログラム')
 st.subheader('左欄からエリアを選択')
 
 # 表記内容
 markdown_main = '''
 【更新内容】\n
-2023-08-14 : 家賃予測プログラムを公開（東京23区、大阪24区、名古屋16区、福岡7区、広島8区）
+2023-08-14 : 家賃推定プログラムを作成（東京23区、大阪24区、名古屋16区、福岡7区、広島8区）
 '''
 st.caption(markdown_main)
